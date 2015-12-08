adfad462fc37bd576b6a6956c7127299ea0f1af4
Ben Marti <kareas1115@gmail.com>
2015-12-07 23:46:32 -0500

    Removed API key

--- core.py
+++ core.py
@@ -1,3 +1,4 @@
+#!/usr/bin/slack-weather
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
@@ -6,16 +7,25 @@ import requests
 
 import pyowm
 
-
+newname = "slack-weather"
+from ctypes import cdll, byref, create_string_buffer
+    
+libc = cdll.LoadLibrary('libc.so.6')    #Loading a 3rd party library C
+buff = create_string_buffer(len(newname)+1) #Note: One larger than the name (man prctl says that)
+buff.value = newname                 #Null terminated string as it should be
+libc.prctl(15, byref(buff), 0, 0, 0) #Refer to "#define" of "/usr/include/linux/prctl.h" for the misterious value 16 & arg[3..5] are zero as the man page says.
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    
-    owm = pyowm.OWM('e9acf144f807e0e7bd7795178be89775')
+def index():
+
+    f = open('apikey.txt', 'r')
+    apikey = f.readline().strip()
+    owm = pyowm.OWM(apikey) 
+
     observation = owm.weather_at_place('Grand Rapids,us')
     w = observation.get_weather()
     t = w.get_temperature('celsius')
@@ -30,21 +40,6 @@ def hello():
     
 
     return w
-
-
-
-@app.route('/getweather')
-def getweather():
-
-
-
-    return "hello"
-    
-
-@app.route('/index')
-def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
